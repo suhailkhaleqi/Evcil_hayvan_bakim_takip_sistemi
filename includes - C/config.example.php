77941c87173556272a6dedf40a065364ebb843ac
suhailkhaleqi <137888795+suhailkhaleqi@users.noreply.github.com>
2025-06-15 12:59:57 +0300

    Add files via upload

---
+++ includes - C/config.example.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$host = "localhost";
+$dbname = "pet_care";
+$username = "root";
+$password = "";
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Veritabanı bağlantısı başarısız: " . $e->getMessage());
+}
+?>
